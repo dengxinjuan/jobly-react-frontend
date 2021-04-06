8dd503b166904a0b9572294f6c6f6b146c4766b0
xinjuan deng <dengxinj@gmail.com>
2021-04-06 15:31:55 -0400

    adding style for jobs

--- src/jobs/JobCard.js
+++ src/jobs/JobCard.js
@@ -1,10 +1,61 @@
-const JobCard = ({ id, title, salary, equity, companyName }) => {
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+const JobCard = ({ id, title, salary, equity, companyName, companyHandle }) => {
+  const classes = useStyles();
   return (
     <div key={id} id={id}>
-      <h1>{title}</h1>
-      {salary}
-      {equity}
-      {companyName}
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            <h2>{title}</h2>
+          </Typography>
+
+          <Typography variant="body2" component="p">
+            Company Name: {companyName}
+            ----
+            <Link to={`/companies/${companyHandle}`}>Go!</Link>
+          </Typography>
+
+          <Typography variant="body2" component="p">
+            salary: ${salary}
+          </Typography>
+          <Typography variant="body2" component="p">
+            equity: {equity}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">
+            <Link>Apply</Link>
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 };
--- src/jobs/JobList.js
+++ src/jobs/JobList.js
@@ -1,9 +1,26 @@
 import JobCard from "./JobCard";
 import JoblyApi from "../api";
 import { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import SearchForm from "../SearchForm";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: 25,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    display: "row",
+  },
+}));
+
 function JobList() {
+  const classes = useStyles();
   const [allJobs, setAllJobs] = useState(null);
 
   useEffect(function getAllJobs() {
@@ -24,18 +41,28 @@ function JobList() {
   if (!allJobs) return "LOADING!";
 
   return (
-    <div>
-      <h1>Search the jobs.</h1>
-      <SearchForm search={search} />
-      {allJobs.map((job) => (
-        <JobCard
-          id={job.id}
-          title={job.title}
-          salary={job.salary}
-          equity={job.equity}
-          companyName={job.companyName}
-        />
-      ))}
+    <div className={classes.root}>
+      <h2>Search the jobs.</h2>
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <SearchForm search={search} />
+          </Paper>
+        </Grid>
+
+        {allJobs.map((job) => (
+          <Grid item xs={3}>
+            <JobCard
+              id={job.id}
+              title={job.title}
+              salary={job.salary}
+              equity={job.equity}
+              companyName={job.companyName}
+              companyHandle={job.companyHandle}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
