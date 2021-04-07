677d7ce77d7175e654f2529428ed6c192af319d9
xinjuan deng <dengxinj@gmail.com>
2021-04-07 16:30:40 -0400

    adding login signup function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import JoblyApi from "./api";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import Routes from "./Routes";
 import { BrowserRouter } from "react-router-dom";
 import NavBar from "./NavBar";
+import jwt from "jsonwebtoken";
 
 function App() {
   const [token, setToken] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+  const [infoLoaded, setInfoLoaded] = useState(false);
 
   /** Handles site-wide signup.
    *
@@ -41,11 +43,46 @@ function App() {
     }
   }
 
+  // Load user info from API. Until a user is logged in and they have a token,
+  // this should not run. It only needs to re-run when a user logs out, so
+  // the value of the token is a dependency for this effect.
+
+  useEffect(
+    function loadUserInfo() {
+      console.debug("App useEffect loadUserInfo", "token=", token);
+
+      async function getCurrentUser() {
+        if (token) {
+          try {
+            let { username } = jwt.decode(token);
+            // put the token on the Api class so it can use it to call the API.
+            JoblyApi.token = token;
+            let currentUser = await JoblyApi.getCurrentUser(username);
+            setCurrentUser(currentUser);
+            //setApplicationIds(new Set(currentUser.applications));
+            console.log(currentUser);
+          } catch (err) {
+            console.error("App loadUserInfo: problem loading", err);
+            setCurrentUser(null);
+          }
+        }
+        setInfoLoaded(true);
+      }
+      // set infoLoaded to false while async getCurrentUser runs; once the
+      // data is fetched (or even if an error happens!), this will be set back
+      // to false to control the spinner.
+      setInfoLoaded(false);
+      getCurrentUser();
+    },
+    [token]
+  );
+
   return (
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
           <NavBar />
+          <h1>Hello! {currentUser ? currentUser.username : "Gergerous"}</h1>
           <Routes login={login} signup={signup} />
         </header>
       </div>
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,5 +1,5 @@
 const HomePage = () => {
-  return <h1>HomePage! Welcome to the jobly!</h1>;
+  return <h1> Welcome to the jobly!</h1>;
 };
 
 export default HomePage;
